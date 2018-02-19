0741b8d107da7c2fd0f54300f9deec26f7f1916b
Derek Ho <derek.ho@gmail.com>
2018-02-18 19:44:26 -0500

    get location

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
 
+const DialogflowApp = require('actions-on-google').DialogflowApp;
+
 const actionHandlers = require('../../pizzabot/src/pizza/action-handlers');
 
 const app = new Koa();
@@ -13,22 +15,71 @@ app
   .use(bodyParser())
   .use(json());
 
-app.use(async (ctx) => {
+function getLocationPermission() {
+  return {
+    "speech": "PLACEHOLDER_FOR_PERMISSION",
+    "contextOut": [],
+    "data": {
+      "google": {
+        "expectUserResponse": true,
+        'isSsml': false,
+        'richResponse': {
+          'items': [
+            {
+              'simpleResponse': {
+                'textToSpeech': 'Need your location',
+                'displayText': 'Need your location'
+              }
+            }
+          ],
+        },
+        'systemIntent': {
+          "intent": "actions.intent.PERMISSION",
+          "data": {
+            "@type": "type.googleapis.com/google.actions.v2.PermissionValueSpec",
+            "optContext": "To deliver your order",
+            "permissions": [
+              "NAME",
+              "DEVICE_PRECISE_LOCATION"
+            ]
+          }
+        }
+      }
+    }
+  };
+};
+
+app.use(async (ctx, next) => {
   try {
-    const { result } = ctx.request.body;
+    const result = ctx.request.body.result;
     const { action } = result;
   
-    if (action === 'RequestPrice') {
-      ctx.body = await actionHandlers.requestPrice(result);
-    } else if (action === 'RequestPrice.OrderConfirm') {
-      ctx.body = await actionHandlers.placeOrder(result);
-    } else if (action === 'FindStore') {
-      ctx.body = await actionHandlers.findStore(result);
+
+    if (action == 'FindStore') {
+      ctx.body = getLocationPermission();
+    } else if (action === 'found_location') {
+      const location = ctx.request.body.originalRequest.data.device.location;
+      console.log(location);
+      ctx.body = {
+        speech: 'Got your location',
+        displayText: JSON.stringify(location),
+      }
     }
+    
+
+    // if (action === 'RequestPrice') {
+    //   ctx.body = await actionHandlers.requestPrice(result);
+    // } else if (action === 'RequestPrice.OrderConfirm') {
+    //   ctx.body = await actionHandlers.placeOrder(result);
+    // } else if (action === 'FindStore') {
+    //   ctx.body = await actionHandlers.findStore(result);
+    // }
   } catch (err) {
     ctx.status = 500;
     ctx.body = { err: `${err}` };
   }
+
+  await next();
 });
 
 const server = http.createServer(app.callback()).listen(3000);
